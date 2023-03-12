f3c80f8d5700b03d64fafa5d7185a0d7868a19cf
zunpher <pattiralekwilai@gmail.com>
2023-03-12 20:57:53 +0700

    EX3

--- Program.cs
+++ Program.cs
@@ -3,18 +3,16 @@
 namespace example_PRG {
     class Program {
         static void Main(string[] args) {
-            Console.Write("Input a: ");
-            double a = double.Parse(Console.ReadLine());
-            Console.Write("Input b: ");
-            double b = double.Parse(Console.ReadLine());
-            Console.Write("Input x: ");
-            double x = double.Parse(Console.ReadLine());
+            Console.Write("Input your n: ");
+            int n = int.Parse(Console.ReadLine());
+            int a, b=1;
 
-            if ( a<=x && x<=b) {
-                Console.WriteLine("True");
-            } else {
-                Console.WriteLine("False");
+            for (a=0; a<n; a++)
+            {
+                b=b*2;
             }
+
+            Console.WriteLine("Sum: {0}", b);
         }
     }
 }
