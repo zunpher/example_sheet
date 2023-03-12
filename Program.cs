e0a1173fe93d1df748be15859cd0a90f169d01db
zunpher <pattiralekwilai@gmail.com>
2023-03-12 16:50:40 +0700

    EX1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace example_PRG {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Please input numner 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Pleasr input number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Please input number 3: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2 + num3;
+            Console.WriteLine("Sum: {0}", sum);
+        }
+    }
+}
